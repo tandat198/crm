3568e0c742ef5e3792428725748ec638b6b62dc7
tandat198 <tandat198@gmail.com>
2020-06-26 15:05:53 +0700

    add short condition

--- playground/condition.js
+++ playground/condition.js
@@ -1,3 +1,23 @@
+// node playground/condition
+
+const isError = true;
+
+if (isError === true) {
+    console.log("Error");
+}
+// Cách viết này có thể viết lại ngắn hơn như sau:
+if (isError) {
+    console.log("Error");
+}
+
+if (isError === false) {
+    console.log("No Error");
+}
+// Cách viết này có thể viết lại như sau:
+if (!isError) {
+    console.log("Error");
+}
+
 // undefined, null, "", 0 tương đương với false
 if (undefined) {
     console.log(undefined);
@@ -18,5 +38,3 @@ if (1) {
 if ("a") {
     console.log("a");
 }
-
-// node playground/condition
